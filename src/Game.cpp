337238c6ff5f209cca3364019ee0ba5275e86f4e
Christian Prince <christianprince132@gmail.com>
2020-10-04 12:09:10 +1100

    Fixed stupid sprite rendering bug

--- src/Game.cpp
+++ src/Game.cpp
@@ -58,12 +58,12 @@ void Game::run()
     Text text(font);
     text.setString("Hello");
     text.setColor(glm::vec3(0.f, 0.f, 0.f));
-    text.setPosition(Vector2f(100, 100));
+    text.setPosition(Vector2f(20, window.getSize().y - 100));
 
     Skybox skybox;
 
     Sprite sprite(ResourceManager::textures.get("crosshair"));
-    sprite.setSize(Vector2f(20, window.getSize().y - 100));
+    sprite.setSize(Vector2f(20, 20));
     sprite.setPosition(Vector2f(window.getSize().x * 0.5f - 25, window.getSize().y * 0.5f - 25));
 
     SpriteRenderer spriteRenderer;
@@ -121,12 +121,12 @@ void Game::run()
         text.setString(std::to_string(fps) + " FPS");
         textRenderer.renderText(text);
 
-        /*textRenderer.renderText(ResourceManager::fonts.get("minecraftia"), 
+        textRenderer.renderText(ResourceManager::fonts.get("minecraftia"), 
                                 std::to_string(fps) + " FPS", 
                                 20, 
                                 window.getSize().y - 100, 
                                 1, 
-                                glm::vec3(0.f, 0.f, 0.f));*/
+                                glm::vec3(0.f, 0.f, 0.f));
         
         window.display();
     }
